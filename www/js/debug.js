bfc16d8127fc2ce80a6e491f40ad52463f4bff2c
applabsllc <58028608+applabsllc@users.noreply.github.com>
2022-05-13 20:53:54 -0500

    ble logic
    added log to handle ble plugin

--- www/js/debug.js
+++ www/js/debug.js
@@ -6,7 +6,7 @@ if(document.getElementById("debugdiv_preload"))
 preloaded=document.getElementById("debugdiv_preload").value;
 
 debughtml+='<div class="debug">';
-debughtml+='<textarea style="width:95%;height:120px;font-size:12px !important;" id="debugdiv">\n'+preloaded+'</textarea>';
+debughtml+='<textarea style="width:95%;height:100px;font-size:12px !important;" id="debugdiv">\n'+preloaded+'</textarea>';
 debughtml+='</div>';
 
 if(debug)
--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,8 @@
 
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
+function onDeviceReady(){
 
     logit('Running cordova-' + cordova.platformId + '@' + cordova.version);
    
@@ -25,6 +26,57 @@ function share(data, url){
 	  console.log("Share failed: " + msg);
 	};
 
-	window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+	window.plugins && window.plugins.socialsharing ? window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError) : onError("no plugin");
 		
+}
+
+function scanBle(){
+	
+	let deviceList = [];
+
+	const ble_success = () => {
+		
+		logit("BT enabled, scanning...");
+		
+		//fetch list of devices
+		//ble.scan(services, seconds, success, failure);
+		
+		ble.startScan([], (device) => {
+			logit(device);
+			deviceList.push(device);
+			redrawList(deviceList);
+		}, () => display_ble("Bluetooth not enabled."));
+		
+	}
+	
+	const ble_failure = () => {
+		//display ble not active
+		display_ble("Bluetooth not enabled.");
+	}
+	
+	ble.isEnabled(ble_success, ble_failure);
+	
+}
+
+function redrawList(deviceList){
+	
+	let listView = "";
+	
+	deviceList.map((dev) => {
+		listView += drawDevice(dev);
+	});
+	
+	displayBle(listView);
+}
+
+function displayBle(str){
+	
+	document.getElementById("deviceList").innerHTML = str;
+	
+}
+
+function drawDevice(obj){
+	
+	return `<div class='device' onClick='pairDevice()'>dev</div>`;
+	
 }
