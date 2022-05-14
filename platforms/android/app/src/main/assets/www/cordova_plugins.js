4f3fc36252056f1d0716e620398beaabe11c8869
applabsllc <58028608+applabsllc@users.noreply.github.com>
2022-05-14 17:48:45 -0500

    logic for ble scan

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-bluetoothle.BluetoothLe",
+      "file": "plugins/cordova-plugin-bluetoothle/www/bluetoothle.js",
+      "pluginId": "cordova-plugin-bluetoothle",
+      "clobbers": [
+        "window.bluetoothle"
+      ]
+    },
+    {
+      "id": "es6-promise-plugin.Promise",
+      "file": "plugins/es6-promise-plugin/www/promise.js",
+      "pluginId": "es6-promise-plugin",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-x-socialsharing.SocialSharing",
+      "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+      "pluginId": "cordova-plugin-x-socialsharing",
+      "clobbers": [
+        "window.plugins.socialsharing"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-bluetoothle": "6.6.2",
+    "cordova-plugin-whitelist": "1.3.5",
+    "es6-promise-plugin": "4.2.2",
+    "cordova-plugin-x-socialsharing": "6.0.3"
+  };
+});
---
+++ platforms/android/app/src/main/assets/www/js/debug.js
@@ -0,0 +1,15 @@
+var debughtml='';
+var preloaded='';
+var debug = true;
+
+if(document.getElementById("debugdiv_preload"))
+preloaded=document.getElementById("debugdiv_preload").value;
+
+debughtml+='<div class="debug">';
+debughtml+='<textarea style="width:95%;height:100px;font-size:12px !important;" id="debugdiv">\n'+preloaded+'</textarea>';
+debughtml+='</div>';
+
+if(debug)
+document.getElementById("notificationArea").innerHTML += debughtml;
+
+			
